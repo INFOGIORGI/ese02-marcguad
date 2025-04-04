533b26c145e398b813a58b5a2e2ab1486a98e2d7
Luciano Schiavone <luciano.schiavone@ittgiorgi.edu.it>
2025-04-04 16:09:14 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Gara gara = new Gara();
+        System.out.println("Vincitore: " + gara.getVincitore());
+        gara.classifica();
     }
 }
